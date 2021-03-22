6b9675c43b4695cf77aab391393891e7ca41e471
Mark Battersby <mark.alfred.battersby@gmail.com>
2021-03-22 13:30:09 -0400

    Allow IPTX import from nested '/extra' path
    Rather than as a sub-module of the main package.
    
    ```ts
    import { IntrospectivePropTypesExtra } from 'introspective-prop-types/extra'
    ```

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 // TS-Style Named Export
 export * from './introspective-prop-types'
-export * from './introspective-prop-types-extra'
 
 // TS-Style Named Export (Alias)
 export { IntrospectivePropTypes as PropTypes } from './introspective-prop-types'
-export { IntrospectivePropTypesExtra as PropTypesExtra } from './introspective-prop-types-extra'
 
 // ES6-Style Default Export
 import { IntrospectivePropTypes } from './introspective-prop-types'
-// import { IntrospectivePropTypesExtra } from './introspective-prop-types-extra'
 export default IntrospectivePropTypes
 
 // ES5-Style Single Export
